f887e806efec4919bf5833899eb145d98b8da935
Matheus Cruz Souza <matheuscruzsouza@gmail.com>
2020-09-23 21:43:14 -0300

    Mapa com marcadores

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
   selector: "app-map",
@@ -6,7 +6,32 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./map.component.sass"],
 })
 export class MapComponent implements OnInit {
+  @ViewChild("canvas", { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  private ctx: CanvasRenderingContext2D;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext("2d");
+
+    this.drawElement(
+      "assets/images/HUD/MAP/vault_not_found_green.png",
+      395.1,
+      121.5
+    );
+  }
+
+  drawElement(src: string, x: number, y: number) {
+    const img = new Image();
+    img.onload = () => {
+      this.drawImageActualSize(img, x, y);
+    };
+    img.src = src;
+  }
+
+  drawImageActualSize(img, x: number, y: number) {
+    this.ctx.drawImage(img, x, y, 32.5, 32.5);
+  }
 }
