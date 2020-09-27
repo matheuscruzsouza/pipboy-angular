e6633cdb6963f2cda58a8cc3d9aca4302c5fbddf
Matheus Cruz Souza <matheuscruzsouza@gmail.com>
2020-09-27 11:54:55 -0300

    Inicio da melhoria do marcador

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  HostListener,
+} from "@angular/core";
 import { DataService } from "src/app/shared/data.service";
 import { Marker } from "src/app/shared/model/marker";
 
@@ -11,44 +17,123 @@ export class MapComponent implements OnInit {
   @ViewChild("canvas", { static: true })
   canvas: ElementRef<HTMLCanvasElement>;
 
-  private ctx: CanvasRenderingContext2D;
+  @ViewChild("destino", { static: true })
+  canvas_destino: ElementRef<HTMLCanvasElement>;
 
-  private TO_RADIANS = Math.PI / 180;
+  private ctx: CanvasRenderingContext2D;
+  private ctx_destino: CanvasRenderingContext2D;
 
   constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
-    this.ctx = this.canvas.nativeElement.getContext("2d");
+  ITEMS = {};
 
-    this.ctx.clearRect(
-      0,
-      0,
-      this.canvas.nativeElement.width,
-      this.canvas.nativeElement.height
+  onScroll(event) {
+    this.clearCanvas(this.ctx_destino);
+
+    this.calculeMidPoint(
+      this.dataService.getPlayerPosition(),
+      this.dataService.getPlayerDestiny()
     );
+  }
 
+  ngOnInit(): void {
+    this.canvas.nativeElement.style.background = `url("/assets/images/map/background_green.png") no-repeat center`;
+
+    this.ctx = this.canvas.nativeElement.getContext("2d");
+    this.ctx_destino = this.canvas_destino.nativeElement.getContext("2d");
+
+    this.clearCanvas();
+
+    this.drawPlayerLocations();
+
+    this.drawPlayerPosition();
+
+    this.drawPlayerDestiny();
+
+    this.calculeMidPoint(
+      this.dataService.getPlayerPosition(),
+      this.dataService.getPlayerDestiny()
+    );
+  }
+
+  clearCanvas(ctx: CanvasRenderingContext2D = this.ctx) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+
+  drawPlayerLocations() {
     this.dataService.getPlayerLocations().forEach((marker) => {
       this.drawElement(marker);
     });
   }
 
-  drawElement(marker: Marker) {
+  drawPlayerPosition() {
+    this.drawElement(this.dataService.getPlayerPosition());
+  }
+
+  drawPlayerDestiny() {
+    this.drawElement(this.dataService.getPlayerDestiny());
+  }
+
+  drawElement(marker: Marker, ctx: CanvasRenderingContext2D = this.ctx) {
     const img = new Image();
     img.onload = () => {
-      this.drawImageActualSize(img, marker);
+      return this.drawImageActualSize(img, marker, ctx);
     };
     img.src = marker.getSrc();
   }
 
-  drawImageActualSize(img, marker: Marker) {
-    marker.width
-      ? this.ctx.drawImage(img, marker.x, marker.y, marker.width, marker.height)
-      : this.ctx.drawImage(
+  drawImageActualSize(img, marker: Marker, ctx: CanvasRenderingContext2D) {
+    const item = marker.width
+      ? ctx.drawImage(
           img,
-          marker.x,
-          marker.y,
+          marker.x - marker.width / 2,
+          marker.y - marker.height / 2,
+          marker.width,
+          marker.height
+        )
+      : ctx.drawImage(
+          img,
+          marker.x - img.width / 6 / 2,
+          marker.y - img.height / 6 / 2,
           img.width / 6,
           img.height / 6
         );
+
+    this.ITEMS[marker.name] = item;
+  }
+
+  calculeMidPoint(a: Marker, b: Marker) {
+    const position = this.calculeFakeMarker();
+
+    const destino = this.dataService.getPlayerDestiny();
+
+    if (position.x + 25 < destino.x && position.y < destino.y) {
+      const fake_marker = new Marker("destiny", position.x, position.y, {
+        width: 12,
+        height: 30,
+      });
+
+      this.drawElement(fake_marker, this.ctx_destino);
+    }
+  }
+
+  calculeFakeMarker() {
+    const parent = this.canvas.nativeElement.parentElement;
+    const left = parent.scrollLeft;
+
+    const player = this.dataService.getPlayerPosition();
+    const destino = this.dataService.getPlayerDestiny();
+
+    const cateto_adjacente = left + destino.x - parent.clientWidth;
+    const angulo_a = Math.atan2(destino.x - player.x, destino.y - player.y);
+    const angulo_b = 90 * (Math.PI / 180) - angulo_a;
+    const cateto_oposto = cateto_adjacente * Math.tan(angulo_b);
+
+    const position = {
+      x: cateto_adjacente - 20,
+      y: cateto_oposto,
+    };
+
+    return position;
   }
 }
--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -21,9 +21,19 @@ export class DataService {
     return [
       new Marker("vault", 395.1, 121.5, { found: true }),
       new Marker("sanctuary", 469, 148, { found: true }),
-      new Marker("player", 370.1, 121.5, { width: 15, height: 20, hud: true }),
       new Marker("hospital", 1173, 371),
-      new Marker("destiny", 1186, 371, { width: 12, height: 30 }),
     ];
+  }
+
+  getPlayerPosition() {
+    return new Marker("player", 370.1, 121.5, {
+      width: 15,
+      height: 20,
+      hud: true,
+    });
+  }
+
+  getPlayerDestiny() {
+    return new Marker("destiny", 1186, 371, { width: 12, height: 30 });
   }
 }
