c62e65c9ede7df0f9f5abda11e233f8d199250ef
Matheus Cruz de Souza <matheuscruzsouza@gmail.com>
2021-04-19 19:25:12 -0300

    Correção da entrada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,30 @@
-import { Component, ViewChild, AfterViewInit } from "@angular/core";
-import { timeout } from "rxjs/operators";
-import { VideoComponent } from "./shared/component/video-player/video.component";
-import { Radio } from "./shared/model/radio";
-import { RadioService } from "./shared/radio.service";
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { timeout } from 'rxjs/operators';
+import { VideoComponent } from './shared/component/video-player/video.component';
+import { Radio } from './shared/model/radio';
+import { RadioService } from './shared/radio.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.sass"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent implements AfterViewInit {
-  title = "pipboy";
+  title = 'pipboy';
 
   radioURL: string;
   videoID: string;
 
   radio = new Audio();
 
-  src = "";
+  src = '';
 
   onBoot = true;
 
   @ViewChild(VideoComponent, { static: false }) video: VideoComponent;
 
   constructor(private radioService: RadioService) {
-    setTimeout((_) => this.onBoot = false, 16000);
+    setTimeout((_) => (this.onBoot = false), 16000);
   }
 
   ngAfterViewInit(): void {
